a0e5b80af2e007d10694ed5adee5ade4cf79672e
Sumbal Khan <sumbal28@live.com>
2023-02-23 14:10:37 +0000

    link deleted path of script

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 let choicesEl = document.querySelector("#choices")
-let questions = [{question: "ask good question", answers:["ex1", "ex2", "ex3", "ex4"], correctAnswer: 3},
-{question: "", answers:["ex1", "ex2", "ex3", "ex4"], correctAnswer: 2}
+let questions = [{question: "Ask good question?", answers:["ex1", "ex2", "ex3", "ex4"], correctAnswer: 3},
+{question: "question 2, 👋 Everyone", answers:["ex1-2", "ex2-2", "ex3-2", "ex4-2"], correctAnswer: 2}
 ]
 
 
 let currentQuestion = 0;
 
 
+
 document.querySelector("#question-title").textContent = questions[0].question
+document.querySelector("#question-title").textContent = questions[1].question
+
+console.log(questions[currentQuestion].answers[0])
+console.log(questions[currentQuestion].answers[1])
+console.log(questions[currentQuestion].answers[2])
+console.log(questions[currentQuestion].answers[3])
 
 
 
@@ -19,4 +26,3 @@ if(event.target.matches("button")){
 console.log(event.target.getAttribute("data-index"))
  }
 })
-alert("hello");
