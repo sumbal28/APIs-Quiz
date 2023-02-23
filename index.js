ec6718ba03cacdb0b27ddea553aa06ac9ccbc5f7
Sumbal Khan <sumbal28@live.com>
2023-02-23 13:45:42 +0000

    check  emoji to test in a browser

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 let choicesEl = document.querySelector("#choices")
-let questions = [{question: "", answers:["ex1", "ex2", "ex3", "ex4"], correctAnswer: 3},
+let questions = [{question: "ask good question", answers:["ex1", "ex2", "ex3", "ex4"], correctAnswer: 3},
 {question: "", answers:["ex1", "ex2", "ex3", "ex4"], correctAnswer: 2}
 ]
 
 
 let currentQuestion = 0;
+
+
+document.querySelector("#question-title").textContent = questions[0].question
+
+
+
 
 
 choicesEl.addEventListener("click", function(event){
@@ -13,3 +19,4 @@ if(event.target.matches("button")){
 console.log(event.target.getAttribute("data-index"))
  }
 })
+alert("hello");
